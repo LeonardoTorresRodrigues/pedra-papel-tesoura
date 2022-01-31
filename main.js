c1507f8987476c61af37ef588c816a6bd7db1c5b
Leonardo Rodrigues <leonardo.t.rodri@gmail.com>
2022-01-31 14:37:16 -0300

    Refazendo estrutura js. Jogo ainda não funcional

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-const pedra = document.querySelector(Pedra);
-const papel = document.querySelector(Papel);
-const tesoura = document.querySelector(Tesoura);
-
-console.log(pedra, papel, tesoura);
+function jogar(escolha) {
+    console.log(escolha);
+}
