3643f1a9a5f518814ff3b680f34041f37dbf4da4
Leonardo Rodrigues <leonardo.t.rodri@gmail.com>
2022-02-01 13:35:50 -0300

    Adicionando variáveis de jogada e logica de jogo do computador

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+let jogadorEscolha = 0;
+let computadorEscolha = 0;
+
 function jogar(escolha) {
-    console.log(escolha);
+    jogadorEscolha = escolha;
+
+    computadorEscolha = Math.floor((Math.random() * (3 - 1 + 1)) + 1);
+    console.log(computadorEscolha)
 }
