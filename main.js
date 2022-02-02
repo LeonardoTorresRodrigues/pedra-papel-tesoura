7729ada76b920d49f30e6ddced632b0967b18f59
Leonardo Rodrigues <leonardo.t.rodri@gmail.com>
2022-02-02 12:23:24 -0300

    Finalizando lógica de jogo. Verificando jogadorEscolha com computadorEscolha

--- main.js
+++ main.js
@@ -5,5 +5,22 @@ function jogar(escolha) {
     jogadorEscolha = escolha;
 
     computadorEscolha = Math.floor((Math.random() * (3 - 1 + 1)) + 1);
-    console.log(computadorEscolha)
+    console.log(computadorEscolha);
+
+    if(jogadorEscolha == computadorEscolha) {
+        alert("Empate! Jogue novamente.");
+    }   else if(jogadorEscolha == 1 && computadorEscolha == 2) {
+            alert("Você perdeu! Computador escolheu Papel.");
+    }       else if(jogadorEscolha == 1 && computadorEscolha == 3) {
+                alert("Você ganhou! Computador escolheu Tesoura");
+    }           else if(jogadorEscolha == 2 && computadorEscolha == 1) {
+                    alert("Você ganhou! Computador escolheu Pedra");
+    }               else if(jogadorEscolha == 2 && computadorEscolha == 3) {
+                        alert("Você perdeu! Computador escolheu Tesoura.");
+    }                   else if(jogadorEscolha == 3 && computadorEscolha == 1) {
+                            alert("Você perdeu! Computador escolheu Pedra");
+    }                       else if(jogadorEscolha == 3 && computadorEscolha == 2) {
+                                alert("Você ganhou! Computador escolheu papel");
+    }
 }
+
