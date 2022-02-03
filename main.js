70ee39f717039b3579fec9770402dd14923ba58d
Leonardo Rodrigues <leonardo.t.rodri@gmail.com>
2022-02-03 14:59:30 -0300

    Finalizando lógica do jogo

--- main.js
+++ main.js
@@ -1,5 +1,48 @@
 let jogadorEscolha = 0;
+let pontosjogador = 0;
 let computadorEscolha = 0;
+let pontosComputador = 0;
+let vencedor = -1;
+
+function pontuacao() {
+    if(vencedor == 0) {
+        document.getElementById('mensagens').innerHTML = 'Empate!';
+    }
+    else if(vencedor == 1) {
+        document.getElementById('mensagens').innerHTML = 'Perdeu!';
+        pontosComputador++;
+    }
+    else if(vencedor == 2) {
+        document.getElementById('mensagens').innerHTML = 'Ganhou!';
+        pontosjogador++;
+    }
+
+    document.getElementById('jogador-pontos').innerHTML = pontosjogador;
+    document.getElementById('computador-pontos').innerHTML = pontosComputador;
+
+    if(pontosjogador == 10) {
+        document.getElementById('mensagens').innerHTML = 'Parabéns! Você é o grande vencedor.';
+        pontosComputador = 0;
+        pontosjogador = 0;
+    }
+    else if(pontosComputador == 10) {
+        document.getElementById('mensagens').innerHTML = 'Ih! O computador foi melhor que você.';
+        pontosComputador = 0;
+        pontosjogador = 0;
+    }
+}
+
+function selecaoDeJogadas() {
+    document.getElementById("jogador-escolha-1").classList.remove('selecionado');
+    document.getElementById("jogador-escolha-2").classList.remove('selecionado');
+    document.getElementById("jogador-escolha-3").classList.remove('selecionado');
+    document.getElementById("computador-escolha-1").classList.remove('selecionado');
+    document.getElementById("computador-escolha-2").classList.remove('selecionado');
+    document.getElementById("computador-escolha-3").classList.remove('selecionado');
+
+    document.getElementById("jogador-escolha-" + jogadorEscolha).classList.add('selecionado');
+    document.getElementById("computador-escolha-" + computadorEscolha).classList.add('selecionado');
+}
 
 function jogar(escolha) {
     jogadorEscolha = escolha;
@@ -8,19 +51,28 @@ function jogar(escolha) {
     console.log(computadorEscolha);
 
     if(jogadorEscolha == computadorEscolha) {
-        alert("Empate! Jogue novamente.");
-    }   else if(jogadorEscolha == 1 && computadorEscolha == 2) {
-            alert("Você perdeu! Computador escolheu Papel.");
-    }       else if(jogadorEscolha == 1 && computadorEscolha == 3) {
-                alert("Você ganhou! Computador escolheu Tesoura");
-    }           else if(jogadorEscolha == 2 && computadorEscolha == 1) {
-                    alert("Você ganhou! Computador escolheu Pedra");
-    }               else if(jogadorEscolha == 2 && computadorEscolha == 3) {
-                        alert("Você perdeu! Computador escolheu Tesoura.");
-    }                   else if(jogadorEscolha == 3 && computadorEscolha == 1) {
-                            alert("Você perdeu! Computador escolheu Pedra");
-    }                       else if(jogadorEscolha == 3 && computadorEscolha == 2) {
-                                alert("Você ganhou! Computador escolheu papel");
+        vencedor = 0;
+    }   
+    else if(jogadorEscolha == 1 && computadorEscolha == 2) {
+        vencedor = 1;
+    }       
+    else if(jogadorEscolha == 1 && computadorEscolha == 3) {
+        vencedor = 2;
+    }           
+    else if(jogadorEscolha == 2 && computadorEscolha == 1) {
+        vencedor = 2;
+    }               
+    else if(jogadorEscolha == 2 && computadorEscolha == 3) {
+        vencedor = 1;
+    }                   
+    else if(jogadorEscolha == 3 && computadorEscolha == 1) {
+        vencedor = 1;
+    }                       
+    else if(jogadorEscolha == 3 && computadorEscolha == 2) {
+        vencedor = 2;
     }
+    
+    selecaoDeJogadas();
+    pontuacao();
 }
 
